99a5809751ecde2e4f801e373f257f143ad49dce
AReynosoV <30306300+AReynosoV@users.noreply.github.com>
2024-09-07 21:57:24 -0400

    Subida Inicial de Mi Sitio Web

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Función que muestra un mensaje de bienvenida
+function mostrarBienvenida() {
+    alert("¡Bienvenido a mi página web!");
+}
+
+// Ejecutar la función cuando la página haya cargado completamente
+window.onload = mostrarBienvenida;
